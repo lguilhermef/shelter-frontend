9787d85f4726060800130926ff170f2505c046a9
Afonso Escudeiro <afonso_escudeiro@hotmail.com>
2022-02-26 19:13:58 -0100

    updated logo to svg

--- src/shared/components/layouts/Main.layout.tsx
+++ src/shared/components/layouts/Main.layout.tsx
@@ -1,47 +1,81 @@
-import { AppBar, Button, Box, IconButton, Toolbar, Typography, Container } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import AddIcon from '@mui/icons-material/Add';
+import {
+    AppBar,
+    Button,
+    Box,
+    IconButton,
+    Toolbar,
+    Typography,
+    Container,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import AddIcon from "@mui/icons-material/Add";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const AppBarTitle: React.FC = ({ children }) => {
-
     const navigate = useNavigate();
 
     const onClickHandler = () => {
         navigate("/");
     };
 
-    return <Typography onClick={onClickHandler}
-        variant="h6"
-        component="div"
-        sx={{ flexGrow: 1, cursor: "pointer" }}>
-        {children}
-    </Typography>;
-}
+    return (
+        <Typography
+            onClick={onClickHandler}
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, cursor: "pointer" }}
+        >
+            {children}
+        </Typography>
+    );
+};
 
-const MainLayout: React.FC = ({children}) => {
-
+const MainLayout: React.FC = ({ children }) => {
     const navigate = useNavigate();
 
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", alignItems: "stretch", height: "100vh" }}>
-            <AppBar sx={{ backgroundColor: "white" }} color="transparent" position="relative">
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "stretch",
+                height: "100vh",
+            }}
+        >
+            <AppBar
+                sx={{ backgroundColor: "white" }}
+                color="transparent"
+                position="relative"
+            >
                 <Container>
                     <Toolbar>
-                        <img height={60} src="/ShelterUkranians_logo.png" alt="" />
+                        <img
+                            height={60}
+                            src="/shelteruktranians_original2.svg"
+                            alt="logo"
+                            style={{ marginRight: "10px" }}
+                        />
                         <AppBarTitle>ShelterUkranians</AppBarTitle>
-                        <Button onClick={ () => { navigate("search-shelter"); }}
-                            sx={{ marginRight: "10px"}}
+                        <Button
+                            onClick={() => {
+                                navigate("search-shelter");
+                            }}
+                            sx={{ marginRight: "10px" }}
                             startIcon={<SearchIcon />}
                             variant="contained"
-                            color="error">
+                            color="error"
+                        >
                             Search Shelter
                         </Button>
-                        <Button onClick={ () => { navigate("add-shelter"); }}
+                        <Button
+                            onClick={() => {
+                                navigate("add-shelter");
+                            }}
                             startIcon={<AddIcon />}
                             variant="contained"
-                            color="success">
+                            color="success"
+                        >
                             Add Shelter
                         </Button>
                     </Toolbar>
